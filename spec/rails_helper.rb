990b7adce22f589e62eca74d032d1239e3c6ff35
Dominik Stodolny <dominik.stodolny@netguru.pl>
2016-02-06 16:32:47 +0100

    add json request helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 require 'rspec/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -15,7 +16,7 @@ end
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Requests::JsonHelpers, type: :request
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
