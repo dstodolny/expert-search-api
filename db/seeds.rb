f1ba6917b39f6d66ecd02e18bf696978d419cfd3
Dominik Stodolny <dominik.stodolny@netguru.pl>
2016-02-06 18:03:11 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+Header.delete_all
+User.delete_all
+
 5.times do
   User.create(name: Faker::Name.name, url: Faker::Internet.url)
+end
+
+User.all.each do |user|
+  5.times do
+    Header.create(
+      user: user,
+      importance: [:h1, :h2, :h3].sample,
+      content: Faker::Lorem.sentence
+    )
+  end
 end
